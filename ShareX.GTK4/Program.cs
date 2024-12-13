5e4bc063941a53c5f2820d5357e9cef03dcf7e84
Brycen G <brycengranville@outlook.com>
2024-12-12 21:12:14 -0500

    fix(GTK4): properly detect GTK version

--- ShareX.GTK4/Program.cs
+++ ShareX.GTK4/Program.cs
@@ -1,22 +1,25 @@
 using Gio;
 using ShareX.GTK4;
 using ShareX.Core.Utils;
+using ShareX.Core;
 
 var shareX = new ShareX.Core.ShareX();
 shareX.setQualifier(" GTK4");
+
 
 
 var application = Gtk.Application.New("ShareX.ShareX", ApplicationFlags.NonUnique);
 application.OnActivate += (sender, eventArgs) =>
 {
     shareX.start(args);
+    DebugHelper.WriteLine("Internal Startup time: {0} ms", shareX.getStartupTime());
+    if (shareX.isSilent()) return;
+
     var dialog = new AboutDialog();
     dialog.SetApplication(application);
-    // I honestly think the bindings don't have this.
-    var gtkVersion = $"Unknown, lololololl";
+    var gtkVersion = $"{Gtk.Functions.GetMajorVersion()}.{Gtk.Functions.GetMinorVersion()}.{Gtk.Functions.GetMicroVersion()}";
     var osInfo = OsInfo.GetFancyOSNameAndVersion();
 
-    // Set system information (including GTK version)
     dialog.SystemInformation = $"OS: {osInfo}\nGTK Version: {gtkVersion}\n.NET Version: {dialog.internalAboutDialog.GetRuntime()}\nPlatform: {dialog.internalAboutDialog.GetOsPlatform()}";
 
     dialog.Show();
@@ -28,5 +31,9 @@ application.OnActivate += (sender, eventArgs) =>
 application.OnShutdown += (sender, eventArgs) =>
 {
     shareX.shutdown();
+};
+application.OnWindowAdded += (sender, eventArgs) =>
+{
+    DebugHelper.WriteLine("Actual UI Startup time: {0} ms", shareX.getStartupTime());
 };
 application.Run(0, args);
